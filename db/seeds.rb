b1264c0e5cea5b23c19042a48eb2aee090492426
hydref <charles.dutheil@gmail.com>
2019-01-30 23:49:39 +0100

    TheHackingPintDONE

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+ user = User.create!(name: Faker::LordOfTheRings.character)
+end
+
+10.times do
+ pin = Pin.create!(url: "https://github.com/stympy/faker", user_id: rand(1..5))
+end
+
+10.times do
+ comment = Comment.create!(content: Faker::Shakespeare.hamlet_quote, user_id: rand(1..5), pin_id: rand(1..10))
+end
